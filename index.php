58f3fbf6fa94036e7de122fa767d466943a67dcb
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-11-28 15:02:45 +0100

    print table

--- index.php
+++ index.php
@@ -54,11 +54,47 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
     <title>Hotels</title>
 </head>
 
 <body>
+    <?php 
+foreach($hotels as $hotel){
+   
 
+    
+    echo"
+    <table class='table'>
+  <thead>
+    <tr>
+      <th scope='col'>name</th>
+      <th scope='col'>description</th>
+      <th scope='col'>parking</th>
+      <th scope='col'>vote</th>
+      <th scope='col'>distance of center</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+    
+      <td>$hotel[name]</td>
+      <td>$hotel[description]</td>
+      <td>$hotel[parking]</td>
+      <td>$hotel[vote]</td>
+      <td>$hotel[distance_to_center] km</td>
+    </tr>
+
+  </tbody>
+</table>
+    ";}
+    
+
+?>
 </body>
 
 </html>
